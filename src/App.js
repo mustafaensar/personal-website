2eea33705b3e61a4b6f61841df2935f73efca9a0
mustafaensar <mustafaensar55@gmail.com>
2023-04-28 20:13:59 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Home from './pages/Home';
+import Header from './layouts/Header';
+import Footer from './layouts/Footer';
+import Resume from './pages/Resume';
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/resume' element={<Resume />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
