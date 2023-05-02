4cff407aa86956708f87845c782744fd1e44da30
mustafaensar <mustafaensar55@gmail.com>
2023-05-02 04:46:15 +0200

    projects page will be hidden only for now

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ export default function Home() {
                   <h1 className="display-3 fw-bolder mb-5"><span className="text-gradient d-inline">Get online and grow fast</span></h1>
                   <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
                     <a className="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="/resume">Resume</a>
-                    <a className="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="/projects">Projects</a>
+                    <a className="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder d-none" href="/projects">Projects</a>
                   </div>
                 </div>
               </div>
